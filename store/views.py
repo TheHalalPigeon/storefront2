f0f9da5ed57bc8deb8a754cd992138535845dfed
Ayat Khaleque Akter <akhalequeakter@gmail.com>
2024-05-20 19:07:55 +0100

    ViewSets

--- store/views.py
+++ store/views.py
@@ -3,23 +3,18 @@ from django.db.models.aggregates import Count
 
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
-    serializer_class = ProductSerializer
-    
-    def get_serializer_context(self):
-        return { "request": self.request } 
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
+class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_serializer_context(self):
+        return { "request": self.request } 
     
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -31,11 +26,7 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(products_count=Count("products")).all()
-    serializer_class = CollectionSerializer
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
     serializer_class = CollectionSerializer
 
